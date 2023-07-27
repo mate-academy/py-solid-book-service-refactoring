a26522492940af5db872c0ad97b87cbd7f710e03
Andriy <andriykrav@gmail.com>
2023-07-27 16:03:41 +0300

    Solution

---
+++ app/display.py
@@ -0,0 +1,20 @@
+class BookDisplay():
+    @staticmethod
+    def display(book, display_type: str) -> None:
+        if display_type == "console":
+            print(book.content)
+        elif display_type == "reverse":
+            print(book.content[::-1])
+        else:
+            raise ValueError(f"Unknown display type: {display_type}")
+
+    @staticmethod
+    def print_book(book, print_type: str) -> None:
+        if print_type == "console":
+            print(f"Printing the book: {book.title}...")
+            print(book.content)
+        elif print_type == "reverse":
+            print(f"Printing the book in reverse: {book.title}...")
+            print(book.content[::-1])
+        else:
+            raise ValueError(f"Unknown print type: {print_type}")

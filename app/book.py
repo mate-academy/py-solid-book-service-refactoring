136d4e18dc4a8c6c991399a177d8c6bcdad0adcd
Volodymyr Panov <vpanov25@gmail.com>
2023-08-03 14:31:33 +0200

    Solution

--- app/book.py
+++ app/book.py
@@ -1,5 +1,5 @@
 import json
-import xml.etree.ElementTree as ET # noqa
+import xml.etree.ElementTree as ET  # noqa
 from abc import ABC, abstractmethod
 
 
@@ -24,45 +24,73 @@ class Validation(ABC):
 class BookValidation(Validation):
     __valid_data = ["console", "reverse", "json", "xml"]
 
-    def check_data_type(self, income_data: str) -> [None, Exception]:
+    def check_data_type(self, income_data: str) -> None:
         if income_data not in self.__valid_data:
             raise ValueError(f"Unknown data type: {income_data}")
 
 
-class BookInitialization:
-    def __init__(self, data_type: str) -> None:
-        self.data_type = data_type
+class ActionWithBookValidation(DoAction, BookValidation):
+    def __init__(self, method_type: str) -> None:
+        self.method_type = method_type
+        self.check_data_type(method_type)
 
 
-class DisplayBook(DoAction, BookInitialization, BookValidation):
+class ConsoleDisplayBook(ActionWithBookValidation):
     def display_book_info(self, book: Book) -> None:
-        self.check_data_type(self.data_type)
-        if self.data_type == "console":
-            print(book.content)
-        elif self.data_type == "reverse":
-            print(book.content[::-1])
+        print(book.content)
 
 
-class PrintBook(DoAction, BookInitialization, BookValidation):
+class ReverseDisplayBook(ActionWithBookValidation):
     def display_book_info(self, book: Book) -> None:
-        self.check_data_type(self.data_type)
-        if self.data_type == "console":
-            print(f"Printing the book: {book.title}...")
-            print(book.content)
-        elif self.data_type == "reverse":
-            print(f"Printing the book in reverse: {book.title}...")
-            print(book.content[::-1])
+        print(book.content[::-1])
 
 
-class SerializeBook(DoAction, BookInitialization, BookValidation):
+class DisplayBook(ActionWithBookValidation):
+    def display_book_info(self, book: Book) -> None:
+        if self.method_type == "console":
+            ConsoleDisplayBook(self.method_type).display_book_info(book)
+        elif self.method_type == "reverse":
+            ReverseDisplayBook(self.method_type).display_book_info(book)
+
+
+class ConsolePrintBook(ActionWithBookValidation):
+    def display_book_info(self, book: Book) -> None:
+        print(f"Printing the book: {book.title}...")
+        print(book.content)
+
+
+class ReversePrintBook(ActionWithBookValidation):
+    def display_book_info(self, book: Book) -> None:
+        print(f"Printing the book in reverse: {book.title}...")
+        print(book.content[::-1])
+
+
+class PrintBook(ActionWithBookValidation):
+    def display_book_info(self, book: Book) -> None:
+        if self.method_type == "console":
+            ConsolePrintBook(self.method_type).display_book_info(book)
+        elif self.method_type == "reverse":
+            ReversePrintBook(self.method_type).display_book_info(book)
+
+
+class SerializeJsonBook(ActionWithBookValidation):
     def display_book_info(self, book: Book) -> str:
-        self.check_data_type(self.data_type)
-        if self.data_type == "json":
-            return json.dumps({"title": book.title, "content": book.content})
-        elif self.data_type == "xml":
-            root = ET.Element("book")
-            title = ET.SubElement(root, "title")
-            title.text = book.title
-            content = ET.SubElement(root, "content")
-            content.text = book.content
-            return ET.tostring(root, encoding="unicode")
+        return json.dumps({"title": book.title, "content": book.content})
+
+
+class SerializeXmlBook(ActionWithBookValidation):
+    def display_book_info(self, book: Book) -> str:
+        root = ET.Element("book")
+        title = ET.SubElement(root, "title")
+        title.text = book.title
+        content = ET.SubElement(root, "content")
+        content.text = book.content
+        return ET.tostring(root, encoding="unicode")
+
+
+class SerializeBook(ActionWithBookValidation):
+    def display_book_info(self, book: Book) -> None | str:
+        if self.method_type == "json":
+            return SerializeJsonBook(self.method_type).display_book_info(book)
+        elif self.method_type == "xml":
+            return SerializeXmlBook(self.method_type).display_book_info(book)

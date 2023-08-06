a40fd95ea6df6b0e81f65a6b6117ec5551b686af
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-06 16:17:13 +0300

    Reduce complexity of main() in main.py

--- app/main.py
+++ app/main.py
@@ -29,19 +29,17 @@ def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
 
     for cmd, method_type in commands:
 
-        if method_type in all_method_types:
-
-            if cmd == "display":
-                displays[method_type].display(book)
-
-            elif cmd == "print":
-                printers[method_type].print(book)
-
-            elif cmd == "serialize":
-                return serializers[method_type].serialize(book)
-
-        else:
+        if method_type not in all_method_types:
             raise ValueError(f"Unknown method entered: {method_type}.")
+
+        if cmd == "display":
+            displays[method_type].display(book)
+
+        elif cmd == "print":
+            printers[method_type].print(book)
+
+        elif cmd == "serialize":
+            return serializers[method_type].serialize(book)
 
 
 if __name__ == "__main__":

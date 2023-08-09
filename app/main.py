c0dc7b2ab9b235e6e0c1c0cabce57369c75965ad
Vyacheslav Kungin <acheros4@gmail.com>
2023-08-09 13:41:14 +0300

    fixed DRY

--- app/main.py
+++ app/main.py
@@ -40,24 +40,24 @@ class PrintBook(ABC):
         self.book = book
 
     @abstractmethod
-    def print_book(self) -> None:
+    def display(self) -> None:
         pass
 
 
 class PrintBookConsole(PrintBook):
-    def print_book(self) -> None:
+    def display(self) -> None:
         print(f"Printing the book: {self.book.title}...")
         print(self.book.content)
 
 
 class PrintBookReverse(PrintBook):
-    def print_book(self) -> None:
+    def display(self) -> None:
         print(f"Printing the book in reverse: {self.book.title}...")
         print(self.book.content[::-1])
 
 
 class PrintBookError(PrintBook):
-    def print_book(self) -> None:
+    def display(self) -> None:
         raise ValueError(f"Unknown print type: {self.print_type}")
 
 
@@ -67,19 +67,19 @@ class Serialize(ABC):
         self.book = book
 
     @abstractmethod
-    def serialize(self) -> str:
+    def display(self) -> str:
         pass
 
 
 class SerializeJson(Serialize):
-    def serialize(self) -> str:
+    def display(self) -> str:
         return json.dumps(
             {"title": self.book.title, "content": self.book.content}
         )
 
 
 class SerializeXml(Serialize):
-    def serialize(self) -> str:
+    def display(self) -> str:
         root = Et.Element("book")
         title = Et.SubElement(root, "title")
         title.text = self.book.title
@@ -89,58 +89,38 @@ class SerializeXml(Serialize):
 
 
 class SerializeError(Serialize):
-    def serialize(self) -> str:
+    def display(self) -> str:
         raise ValueError(f"Unknown serialize type: {self.serialize_type}")
 
 
-def display(book: Book, method_type: str) -> None:
-    match method_type:
-        case "console":
-            display_console = DisplayConsole(book, method_type)
-            display_console.display()
-        case "reverse":
-            display_reverse = DisplayReverse(book, method_type)
-            display_reverse.display()
-        case None:
-            display_error = DisplayError(book, method_type)
-            display_error.display()
+ACTION_TYPES = {
+    "display": {
+        "console": DisplayConsole,
+        "reverse": DisplayReverse,
+        None: DisplayError
+    },
+    "print": {
+        "console": PrintBookConsole,
+        "reverse": PrintBookReverse,
+        None: PrintBookError
+    },
+    "serialize": {
+        "json": SerializeJson,
+        "xml": SerializeXml,
+        None: SerializeError
+    }
+}
 
 
-def print_book(book: Book, method_type: str) -> None:
-    match method_type:
-        case "console":
-            print_book_console = PrintBookConsole(book, method_type)
-            print_book_console.print_book()
-        case "reverse":
-            print_book_console = PrintBookReverse(book, method_type)
-            print_book_console.print_book()
-        case None:
-            print_book_console = PrintBookError(book, method_type)
-            print_book_console.print_book()
+def perform_action(action_type: str, book: Book, method_type: str):
+    action = ACTION_TYPES[action_type][method_type](book, method_type)
+    return action.display()
 
 
-def serialize(book: Book, method_type: str) -> None | str:
-    match method_type:
-        case "json":
-            serialize_json = SerializeJson(book, method_type)
-            return serialize_json.serialize()
-        case "xml":
-            serialize_xml = SerializeXml(book, method_type)
-            return serialize_xml.serialize()
-        case None:
-            serialize_error = SerializeError(book, method_type)
-            return serialize_error.serialize()
-
-
-def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
-    for cmd, method_type in commands:
-        if cmd == "display":
-            display(book, method_type)
-        elif cmd == "print":
-            print_book(book, method_type)
-        elif cmd == "serialize":
-            return serialize(book, method_type)
-
+def main(book: Book, commands: list[tuple[str, str]]) -> None:
+    for action_type, method_type in commands:
+        if action_type in ["display", "print", "serialize"]:
+            return perform_action(action_type, book, method_type)
 
 
 if __name__ == "__main__":

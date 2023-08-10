132dc622b59ac431195f0913893fa31af063eb37
AnDy <ansmbox@gmail.com>
2023-08-10 22:38:47 +0300

    Refactor class Book()

--- app/book.py
+++ app/book.py
@@ -1,25 +1,4 @@
-from app.book_display import BOOK_DISPLAY_PROCESSORS
-from app.book_printer import BOOK_PRINT_PROCESSORS
-from app.book_serializer import BOOK_SERIALIZE_PROCESSORS
-
-
 class Book:
-    BOOK_PROCESSORS = {
-        "display": BOOK_DISPLAY_PROCESSORS,
-        "print": BOOK_PRINT_PROCESSORS,
-        "serialize": BOOK_SERIALIZE_PROCESSORS,
-    }
-
     def __init__(self, title: str, content: str) -> None:
         self.title = title
         self.content = content
-
-    def handle(self, command: str, method: str) -> None:
-        if command in self.BOOK_PROCESSORS:
-            if method in self.BOOK_PROCESSORS[command]:
-                handler = getattr(
-                    self.BOOK_PROCESSORS[command][method](), command
-                )
-                return handler(self.title, self.content)
-            raise ValueError(f"Unknown method: {method}")
-        raise ValueError(f"Unknown command: {command}")
--- app/main.py
+++ app/main.py
@@ -1,9 +1,31 @@
 from app.book import Book
+from app.book_display import BOOK_DISPLAY_PROCESSORS
+from app.book_printer import BOOK_PRINT_PROCESSORS
+from app.book_serializer import BOOK_SERIALIZE_PROCESSORS
+
+
+BOOK_PROCESSORS = {
+    "display": BOOK_DISPLAY_PROCESSORS,
+    "print": BOOK_PRINT_PROCESSORS,
+    "serialize": BOOK_SERIALIZE_PROCESSORS,
+
+}
+
+
+def handle_book(book: Book, command: str, method: str) -> None:
+    if command in BOOK_PROCESSORS:
+        if method in BOOK_PROCESSORS[command]:
+            handler = getattr(
+                BOOK_PROCESSORS[command][method](), command
+            )
+            return handler(book.title, book.content)
+        raise ValueError(f"Unknown method: {method}")
+    raise ValueError(f"Unknown command: {command}")
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for command, method in commands:
-        return book.handle(command, method)
+        return handle_book(book, command, method)
 
 
 if __name__ == "__main__":

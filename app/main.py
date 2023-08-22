cef60778f9b8763beb37976e138d550050e53495
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-23 00:36:45 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,52 +1,43 @@
-import json
-import xml.etree.ElementTree as ET
-
-
-class Book:
-    def __init__(self, title: str, content: str):
-        self.title = title
-        self.content = content
-
-    def display(self, display_type: str) -> None:
-        if display_type == "console":
-            print(self.content)
-        elif display_type == "reverse":
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown display type: {display_type}")
-
-    def print_book(self, print_type: str) -> None:
-        if print_type == "console":
-            print(f"Printing the book: {self.title}...")
-            print(self.content)
-        elif print_type == "reverse":
-            print(f"Printing the book in reverse: {self.title}...")
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown print type: {print_type}")
-
-    def serialize(self, serialize_type: str) -> str:
-        if serialize_type == "json":
-            return json.dumps({"title": self.title, "content": self.content})
-        elif serialize_type == "xml":
-            root = ET.Element("book")
-            title = ET.SubElement(root, "title")
-            title.text = self.title
-            content = ET.SubElement(root, "content")
-            content.text = self.content
-            return ET.tostring(root, encoding="unicode")
-        else:
-            raise ValueError(f"Unknown serialize type: {serialize_type}")
+from app.book import Book
+from app.displaying import (
+    BookDisplayConsole,
+    BookDisplayReverse
+)
+from app.printing import (
+    BookPrintConsole,
+    BookPrintReverse
+)
+from app.serializing import (
+    BookSerializeJSON,
+    BookSerializeXML
+)
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
+    displaying = {
+        "console": BookDisplayConsole(),
+        "reverse": BookDisplayReverse(),
+    }
+
+    printing = {
+        "console": BookPrintConsole(),
+        "reverse": BookPrintReverse(),
+    }
+
+    serialization = {
+        "json": BookSerializeJSON(),
+        "xml": BookSerializeXML(),
+    }
     for cmd, method_type in commands:
         if cmd == "display":
-            book.display(method_type)
+            displaying[method_type].display(book.content)
         elif cmd == "print":
-            book.print_book(method_type)
+            printing[method_type].print(book.title, book.content)
         elif cmd == "serialize":
-            return book.serialize(method_type)
+            return serialization[method_type].serialize(
+                book.title,
+                book.content
+            )
 
 
 if __name__ == "__main__":

e6cdbf3c05b8ff5d15d915fc90580dbefb447c53
Yaroslav Konoplyov <kannbis252@gmail.com>
2023-08-22 16:43:48 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,52 +1,71 @@
 import json
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ETree
+from abc import abstractmethod, ABC
 
 
 class Book:
-    def __init__(self, title: str, content: str):
+    def __init__(self, title: str, content: str) -> None:
         self.title = title
         self.content = content
 
-    def display(self, display_type: str) -> None:
+
+class Display:
+    @staticmethod
+    def display(book: Book, display_type: str) -> None:
         if display_type == "console":
-            print(self.content)
+            print(book.content)
         elif display_type == "reverse":
-            print(self.content[::-1])
+            print(book.content[::-1])
         else:
             raise ValueError(f"Unknown display type: {display_type}")
 
-    def print_book(self, print_type: str) -> None:
+
+class PrintBook:
+    @staticmethod
+    def print(book: Book, print_type: str) -> None:
         if print_type == "console":
-            print(f"Printing the book: {self.title}...")
-            print(self.content)
+            print(f"Printing the book: {book.title}...")
+            print(book.content)
         elif print_type == "reverse":
-            print(f"Printing the book in reverse: {self.title}...")
-            print(self.content[::-1])
+            print(f"Printing the book in reverse: {book.title}...")
+            print(book.content[::-1])
         else:
             raise ValueError(f"Unknown print type: {print_type}")
 
-    def serialize(self, serialize_type: str) -> str:
-        if serialize_type == "json":
-            return json.dumps({"title": self.title, "content": self.content})
-        elif serialize_type == "xml":
-            root = ET.Element("book")
-            title = ET.SubElement(root, "title")
-            title.text = self.title
-            content = ET.SubElement(root, "content")
-            content.text = self.content
-            return ET.tostring(root, encoding="unicode")
-        else:
-            raise ValueError(f"Unknown serialize type: {serialize_type}")
+
+class Serializer(ABC):
+    @abstractmethod
+    def serialize(self, book: Book) -> str:
+        pass
+
+
+class JSONSerializer(Serializer):
+    def serialize(self, book: Book) -> str:
+        return json.dumps({"title": book.title, "content": book.content})
+
+
+class XMLSerializer(Serializer):
+    def serialize(self, book: Book) -> str:
+        root = ETree.Element("book")
+        title = ETree.SubElement(root, "title")
+        title.text = book.title
+        content = ETree.SubElement(root, "content")
+        content.text = book.content
+        return ETree.tostring(root, encoding="unicode")
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
+    serializers = {
+        "json": JSONSerializer,
+        "xml": XMLSerializer
+    }
     for cmd, method_type in commands:
         if cmd == "display":
-            book.display(method_type)
+            Display.display(book, method_type)
         elif cmd == "print":
-            book.print_book(method_type)
+            PrintBook.print(book, method_type)
         elif cmd == "serialize":
-            return book.serialize(method_type)
+            return serializers.get(method_type)().serialize(book)
 
 
 if __name__ == "__main__":

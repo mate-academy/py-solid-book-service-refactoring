36a853797b817c308d0254de598c02e06fb26818
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-23 11:53:53 +0300

    added wrong input handling

--- app/main.py
+++ app/main.py
@@ -18,14 +18,24 @@ def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
         "xml": XmlSerialize(),
     }
     for cmd, method_type in commands:
-        if cmd == "display":
-            displayers[method_type].display(book.content)
-        elif cmd == "print":
-            printers[method_type].print_book(book.title, book.content)
-        elif cmd == "serialize":
-            return serializers[method_type].serialize(book.title, book.content)
+        try:
+            if cmd == "display":
+                displayers[method_type].display(book.content)
+            elif cmd == "print":
+                printers[method_type].print_book(book.title, book.content)
+            elif cmd == "serialize":
+                return serializers[method_type].serialize(
+                    book.title,
+                    book.content
+                )
+            else:
+                raise ValueError
+        except ValueError as e:
+            print(f"{type(e).__name__}: Unknown method - {cmd}.")
+        except KeyError as e:
+            print(f"{type(e).__name__}: Unknown method type - {method_type}.")
 
 
 if __name__ == "__main__":
     sample_book = Book("Sample Book", "This is some sample content.")
-    print(main(sample_book, [("display", "reverse"), ("serialize", "xml")]))
+    print(main(sample_book, [("print", "console"), ("serialize", "xml")]))

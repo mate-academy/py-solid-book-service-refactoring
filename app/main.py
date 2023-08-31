4eab43d24e0d00811fcf8679444bbc53fb50967b
leetwinoff <leetwinoff92@gmail.com>
2023-08-31 15:38:22 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -8,27 +8,27 @@ from app.serializers import JsonSerialize, XmlSerialize
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     displays = {
-        "console": ConsoleDisplay(),
-        "reverse": ReverseDisplay(),
+        "console": ConsoleDisplay,
+        "reverse": ReverseDisplay,
     }
 
     printers = {
-        "console": ConsolePrint(),
-        "reverse": ReversePrint(),
+        "console": ConsolePrint,
+        "reverse": ReversePrint,
     }
 
     serialization = {
-        "json": JsonSerialize(),
-        "xml": XmlSerialize(),
+        "json": JsonSerialize,
+        "xml": XmlSerialize,
     }
 
     for cmd, method_type in commands:
         if cmd == "display":
-            displays[method_type].display(book.content)
+            displays[method_type]().display(book.content)
         elif cmd == "print":
-            printers[method_type].print(book.title, book.content)
+            printers[method_type]().print(book.title, book.content)
         elif cmd == "serialize":
-            return serialization[method_type].serialize(
+            return serialization[method_type]().serialize(
                 book.title,
                 book.content
             )

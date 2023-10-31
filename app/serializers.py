4bc9be4ad03869b5bf8cac594bb49724ceb03157
eduardhabryd <eduardhabryd@gmail.com>
2023-10-31 21:38:53 +0200

    implement separate class for each serializer type

---
+++ app/serializers.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+import json
+import xml.etree.ElementTree as ET
+
+
+class SerializerStrategy(ABC):
+    @abstractmethod
+    def serialize(self, title: str, content: str) -> str:
+        pass
+
+
+class JsonSerializer(SerializerStrategy):
+    def serialize(self, title: str, content: str) -> str:
+        return json.dumps({"title": title, "content": content})
+
+
+class XmlSerializer(SerializerStrategy):
+    def serialize(self, title: str, content: str) -> str:
+        root = ET.Element("book")
+        title_element = ET.SubElement(root, "title")
+        title_element.text = title
+        content_element = ET.SubElement(root, "content")
+        content_element.text = content
+        return ET.tostring(root, encoding="unicode")

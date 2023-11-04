2a415145236d6e6bfe470058c345c3d71d527d0b
Andy <andy77andy@ukr.net>
2023-11-04 23:49:25 +0200

    Edits

---
+++ app/print_manager.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+from app.book import Book
+
+
+class PrintBook(ABC):
+    @abstractmethod
+    def print_book(self) -> None:
+        pass
+
+
+class PrintBookConsole(PrintBook):
+
+    def __init__(self, book: Book) -> None:
+        self.content = book.content
+        self.method_type = "console"
+        self.message = f"Printing the book: {book.title}..."
+
+    def print_book(self) -> None:
+        print(self.message)
+        print(self.content)
+
+
+class PrintBookReverse(PrintBook):
+
+    def __init__(self, book: Book) -> None:
+        self.content = book.content
+        self.method_type = "reverse"
+        self.message = f"Printing the book in reverse: {book.title}..."
+
+    def print_book(self) -> None:
+        print(self.message)
+        print(self.content[::-1])
---
+++ app/serialize_manager.py
@@ -0,0 +1,37 @@
+import json
+from abc import ABC, abstractmethod
+import xml.etree.ElementTree as ET
+
+from app.book import Book
+
+
+class Serialize(ABC):
+    @abstractmethod
+    def serialize_book(self,):
+        pass
+
+
+class SerializeJson(Serialize):
+
+    def __init__(self, book: Book) -> None:
+        self.method_type = "json"
+        self.title = book.title
+        self.content = book.content
+
+    def serialize_book(self)-> str:
+        return json.dumps({"title": self.title, "content": self.content})
+
+
+class SerializeXML(Serialize):
+    def __init__(self, book: Book) -> None:
+        self.method_type = "json"
+        self.title = book.title
+        self.content = book.content
+
+    def serialize_book(self) -> str:
+        root = ET.Element("book")
+        title = ET.SubElement(root, "title")
+        title.text = self.title
+        content = ET.SubElement(root, "content")
+        content.text = self.content
+        return ET.tostring(root, encoding="unicode")

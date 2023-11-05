c9a4db70a5e619707cad17ca20267ed0424207ec
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-11-05 23:29:56 +0200

    Fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ from app.book_print import PrintBook
 from app.book_serializer import SerializerFormat
 
 
-def run_display(book: Book, method_type: str):
+def run_display(book: Book, method_type: str) -> None:
     book.display(method_type)
 
 
-def run_print(book: Book, method_type: str):
+def run_print(book: Book, method_type: str) -> None:
     PrintBook(book).print(method_type)
 
 
-def run_serialize(book: Book, method_type: str):
+def run_serialize(book: Book, method_type: str) -> str:
     serializer = SerializerFormat.create_serializer(method_type, book)
     return serializer.serialize()
 

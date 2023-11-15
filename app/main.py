4a3ff467a0e43229fc8c398bc5921291e0f36404
ORENYT <nkforworkmail@gmail.com>
2023-11-15 23:04:32 +0300

    Used command_map mapper to optimize code

--- app/main.py
+++ app/main.py
@@ -3,13 +3,15 @@ from app.commands import DisplayCommand, SerializeCommand, PrintCommand
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
+    command_map = {
+        "display": DisplayCommand,
+        "print": PrintCommand,
+        "serialize": SerializeCommand
+    }
     for cmd, method_type in commands:
-        if cmd == "display":
-            DisplayCommand(method_type, book).execute()
-        if cmd == "print":
-            PrintCommand(method_type, book).execute()
-        if cmd == "serialize":
-            return SerializeCommand(method_type, book).execute()
+        command_class = command_map.get(cmd)
+        command = command_class(method_type, book)
+        return command.execute()
 
 
 if __name__ == "__main__":

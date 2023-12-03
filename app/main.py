2db2ee10afb33f5d9d9c1e7045e979ebb6ce1b5b
Paul Starodub <amorallex@gmail.com>
2023-12-03 14:49:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
+from app.display import DisplayConsole, DisplayReverse
+from app.printed import PrintConsole, PrintReverse
 from app.serializer import JsonSerializer, XmlSerializer
 from app.book import Book
 
-
-serialize_type = {"json": JsonSerializer, "xml": XmlSerializer}
+all_data = {
+    "serialize_type": {"json": JsonSerializer, "xml": XmlSerializer},
+    "display_type": {"console": DisplayConsole, "reverse": DisplayReverse},
+    "print_type": {"console": PrintConsole, "reverse": PrintReverse},
+}
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
         if cmd == "display":
-            book.display(method_type)
+            all_data["display_type"].get(method_type)(book).display()
         elif cmd == "print":
-            book.print_book(method_type)
+            all_data["print_type"].get(method_type)(book).print_book()
         elif cmd == "serialize":
-            return serialize_type.get(method_type)(book).serialize()
+            return (
+                all_data["serialize_type"].get(method_type)(book).serialize()
+            )
 
 
 if __name__ == "__main__":

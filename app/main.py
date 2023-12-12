fcf04d666f0c5c8fa18e2cdec2927d09891dee19
Taras Savchyn <savik1992@gmail.com>
2023-12-12 23:12:36 +0200

    add new file book.py and relocate logiс into file

--- app/main.py
+++ app/main.py
@@ -1,34 +1,5 @@
+from app.book import Book, BookDisplay, BookPrinter
 from app.serializers import BookSerializer
-
-
-class Book:
-    def __init__(self, title: str, content: str) -> None:
-        self.title = title
-        self.content = content
-
-
-class BookDisplay:
-    @staticmethod
-    def display(book: Book, display_type: str) -> None:
-        if display_type == "console":
-            print(book.content)
-        elif display_type == "reverse":
-            print(book.content[::-1])
-        else:
-            raise ValueError(f"Unknown display type: {display_type}")
-
-
-class BookPrinter:
-    @staticmethod
-    def print_book(book: Book, print_type: str) -> None:
-        if print_type == "console":
-            print(f"Printing the book: {book.title}...")
-            print(book.content)
-        elif print_type == "reverse":
-            print(f"Printing the book in reverse: {book.title}...")
-            print(book.content[::-1])
-        else:
-            raise ValueError(f"Unknown print type: {print_type}")
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:

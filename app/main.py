fd3ba82538c79966155117e470ffd819d9dcafe8
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-22 18:21:12 +0200

    pass test

--- app/main.py
+++ app/main.py
@@ -38,25 +38,28 @@ class DisplayManager:
 
 class Print(ABC):
     @abstractmethod
-    def print_book(self, title) -> None:
+    def print_book(self, title, content) -> None:
         pass
 
 
-class PrintConsole(Print):
-    def print_book(self, title) -> None:
+class PrintConsole(Print, DisplayConsole):
+    def print_book(self, title, content) -> None:
         print(f"Printing the book: {title}...")
+        self.display(content)
 
 
-class PrintReverse(Print):
-    def print_book(self, title) -> None:
+class PrintReverse(Print, DisplayReverse):
+    def print_book(self, title, content) -> None:
         print(f"Printing the book in reverse: {title}...")
+        self.display(content)
 
 
 class PrintManager:
     print_managers = {}
 
-    def __init__(self, title):
+    def __init__(self, title, content):
         self.title = title
+        self.content = content
 
         self.print_managers["console"] = PrintConsole()
         self.print_managers["reverse"] = PrintReverse()
@@ -64,38 +67,37 @@ class PrintManager:
     def print_book(self, method_type) -> None:
         print_manager = self.print_managers.get(method_type)
         if print_manager:
-            print_manager.print_book(self.title)
+            print_manager.print_book(self.title, self.content)
         else:
             print(f"Invalid method_type: {method_type}")
 
 
 class Serialize(ABC):
     @abstractmethod
-    def serialize(self, title, content) -> str:
+    def serialize(self, book) -> str:
         pass
 
 
 class SerializeJson(Serialize):
-    def serialize(self, title, content) -> str:
-        return json.dumps({"title": title, "content": content})
+    def serialize(self, book) -> str:
+        return json.dumps({"title": book.title, "content": book.content})
 
 
 class SerializeXml(Serialize):
-    def serialize(self, title, content) -> str:
+    def serialize(self, book) -> str:
         root = ET.Element("book")
         title = ET.SubElement(root, "title")
-        title.text = title
+        title.text = book.title
         content = ET.SubElement(root, "content")
-        content.text = content
+        content.text = book.content
         return ET.tostring(root, encoding="unicode")
 
 
 class SerializeManager:
     serialize_managers = {}
 
-    def __init__(self, title: str, content: str):
-        self.title = title
-        self.content = content
+    def __init__(self, book):
+        self.book = book
 
         self.serialize_managers["json"] = SerializeJson()
         self.serialize_managers["xml"] = SerializeXml()
@@ -103,16 +105,16 @@ class SerializeManager:
     def serialize(self, method_type) -> str:
         serialize_manager = self.serialize_managers.get(method_type)
         if serialize_manager:
-            return serialize_manager.serialize(self.title, self.content)
+            return serialize_manager.serialize(self.book)
         else:
-            return f"Invalid method_type: {method_type}"
+            print(f"Invalid method_type: {method_type}")
 
 
 class Book(DisplayManager, PrintManager, SerializeManager):
     def __init__(self, title: str, content: str):
         DisplayManager.__init__(self, content)
-        PrintManager.__init__(self, title)
-        SerializeManager.__init__(self, title, content)
+        PrintManager.__init__(self, title, content)
+        SerializeManager.__init__(self, self)
         self.title = title
         self.content = content
 

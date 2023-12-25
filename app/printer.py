fd0a24fae493635ac7a584883fc5435b1bcf6d68
Rost <rv4@ukr.net>
2023-12-25 13:42:40 +0200

    add files

---
+++ app/printer.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+from app.book import Book
+
+
+class Printer(ABC):
+    def __init__(self, book: Book) -> None:
+        self.book = book
+
+    @abstractmethod
+    def print(self) -> None:
+        pass
+
+
+class ConsolePrinter(Printer):
+    def print(self) -> None:
+        print(f"Printing the book: {self.book.title}...")
+        print(self.book.content)
+
+
+class ReversePrinter(Printer):
+    def print(self) -> None:
+        print(f"Printing the book in reverse: {self.book.title}...")
+        print(self.book.content[::-1])
---
+++ app/serializer.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+import json
+import xml.etree.ElementTree as Et
+
+from app.book import Book
+
+
+class Serializer(ABC):
+    def __init__(self, book: Book) -> None:
+        self.book = book
+
+    @abstractmethod
+    def serialize(self) -> None:
+        pass
+
+
+class JSONSerializer(Serializer):
+    def serialize(self) -> json:
+        return json.dumps({"title": self.book.title,
+                           "content": self.book.content})
+
+
+class XMLSerializer(Serializer):
+    def serialize(self) -> Et:
+        root = Et.Element("book")
+        title = Et.SubElement(root, "title")
+        title.text = self.book.title
+        content = Et.SubElement(root, "content")
+        content.text = self.book.content
+        return Et.tostring(root, encoding="unicode")

08bb04dd4aed6ea22fa073bf1fa7066dc21b6cbe
maxitoska <maxitosm00@gmail.com>
2024-01-09 23:01:18 +0200

    This code follow the rule of LSP principle

--- app/main.py
+++ app/main.py
@@ -11,58 +11,75 @@ class Book:
 
 class BookDisplay(ABC):
     @abstractmethod
-    def display(self, book: Book, display_type: str) -> None:
+    def display(self, book: Book) -> None:
         pass
 
 
 class DisplayConsoleProcessor(BookDisplay):
-    def display(self, book: Book, display_type: str) -> None:
-        if display_type == "console":
+    def __init__(self, display_type: str) -> None:
+        self.display_type = display_type
+
+    def display(self, book: Book) -> None:
+        if self.display_type == "console":
             print(book.content)
 
 
 class DisplayReverseProcessor(BookDisplay):
-    def display(self, book: Book, display_type: str) -> None:
-        if display_type == "reverse":
+    def __init__(self, display_type: str) -> None:
+        self.display_type = display_type
+
+    def display(self, book: Book) -> None:
+        if self.display_type == "reverse":
             print(book.content[::-1])
 
 
 class BookPrinter(ABC):
     @abstractmethod
-    def print_book(self, book: Book, print_type: str) -> None:
+    def print_book(self, book: Book) -> None:
         pass
 
 
 class PrinterConsoleProcessor(BookPrinter):
+    def __init__(self, print_type: str) -> None:
+        self.print_type = print_type
 
-    def print_book(self, book: Book, print_type: str) -> None:
-        if print_type == "console":
+    def print_book(self, book: Book) -> None:
+        if self.print_type == "console":
             print(f"Printing the book: {book.title}...")
             print(book.content)
 
 
 class PrinterReverseProcessor(BookPrinter):
-    def print_book(self, book: Book, print_type: str) -> None:
-        if print_type == "reverse":
+    def __init__(self, print_type: str) -> None:
+        self.print_type = print_type
+
+    def print_book(self, book: Book) -> None:
+        if self.print_type == "reverse":
             print(f"Printing the book in reverse: {book.title}...")
             print(book.content[::-1])
 
 
 class BookSerializer(ABC):
     @abstractmethod
-    def serialize(self, book: Book, serialize_type: str) -> str:
-        raise ValueError(f"Unknown serialize type: {serialize_type}")
+    def serialize(self, book: Book) -> None:
+        pass
 
 
 class SerializerJson(BookSerializer):
-    def serialize(self, book: Book, serialize_type: str) -> str:
-        if serialize_type == "json":
+    def __init__(self, serialize_type: str) -> None:
+        self.serialize_type = serialize_type
+
+    def serialize(self, book: Book) -> str:
+        if self.serialize_type == "json":
             return json.dumps({"title": book.title, "content": book.content})
 
 
 class SerializerXML(BookSerializer):
-    def serialize(self, book: Book, serialize_type: str) -> str:
-        if serialize_type == "xml":
+    def __init__(self, serialize_type: str) -> None:
+        self.serialize_type = serialize_type
+
+    def serialize(self, book: Book) -> str:
+        if self.serialize_type == "xml":
             root = ElementTree.Element("book")
             title = ElementTree.SubElement(root, "title")
             title.text = book.title
@@ -74,37 +91,37 @@ class SerializerXML(BookSerializer):
 # class SerializerHandler(SerializerJson, SerializerXML)
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     display_processors = {
-        "console": DisplayConsoleProcessor(),
-        "reverse": DisplayReverseProcessor(),
+        "console": DisplayConsoleProcessor("console"),
+        "reverse": DisplayReverseProcessor("reverse"),
     }
 
     printer_processors = {
-        "console": PrinterConsoleProcessor(),
-        "reverse": PrinterReverseProcessor(),
+        "console": PrinterConsoleProcessor("console"),
+        "reverse": PrinterReverseProcessor("reverse"),
     }
 
     serializer_processors = {
-        "json": SerializerJson(),
-        "xml": SerializerXML(),
+        "json": SerializerJson("json"),
+        "xml": SerializerXML("xml"),
     }
 
     for cmd, method_type in commands:
         if cmd == "display":
             display_processor = display_processors.get(method_type)
             if display_processor:
-                display_processor.display(book, method_type)
+                display_processor.display(book)
             else:
                 raise ValueError(f"Unknown display type: {method_type}")
         elif cmd == "print":
             printer_processor = printer_processors.get(method_type)
             if printer_processor:
-                printer_processor.print_book(book, method_type)
+                printer_processor.print_book(book)
             else:
                 raise ValueError(f"Unknown print type: {method_type}")
         elif cmd == "serialize":
             serializer_processor = serializer_processors.get(method_type)
             if serializer_processor:
-                return serializer_processor.serialize(book, method_type)
+                return serializer_processor.serialize(book)
             else:
                 raise ValueError(f"Unknown serialize type: {method_type}")
 

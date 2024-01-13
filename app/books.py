90e88b6c86aff7749c94c7d77ce615e94fa4e7d9
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2024-01-13 14:30:28 +0200

    added to class Book setters and getters

--- app/books.py
+++ app/books.py
@@ -1,4 +1,20 @@
 class Book:
-    def __init__(self, title: str, content: str):
-        self.title = title
-        self.content = content
+    def __init__(self, title: str, content: str) -> None:
+        self._title = title
+        self._content = content
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @title.setter
+    def title(self, value: str) -> None:
+        self._title = value
+
+    @property
+    def content(self) -> str:
+        return self._content
+
+    @content.setter
+    def content(self, value: str) -> None:
+        self._content = value

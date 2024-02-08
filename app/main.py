91db0c46830109fd255cd6874010ef1f7cfa5d20
Bohdan Marko <goldenbohdanm@gmail.com>
2024-02-08 18:30:19 +0200

    Follow SRP, make seperate classes for different actions

--- app/main.py
+++ app/main.py
@@ -1,14 +1,45 @@
 from app.book import Book
+from app.displayer import DisplayBookConsole, DisplayBookReverse
+from app.printbook import PrintBookConsole, PrintBookReverse
+from app.serializer import BookSerializerXML, BookSerializerJSON
+
+
+interface = {
+    "display": {
+        "console": DisplayBookConsole,
+        "reverse": DisplayBookReverse,
+    },
+    "print": {
+        "console": PrintBookConsole,
+        "reverse": PrintBookReverse,
+    },
+    "serialize": {
+        "json": BookSerializerJSON,
+        "xml": BookSerializerXML,
+    }
+}
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
-        if cmd == "display":
-            book.display_book(method_type)
-        elif cmd == "print":
-            book.print_book(method_type)
-        elif cmd == "serialize":
-            return book.serialize(method_type)
+        if interface.get(cmd):
+            if cmd == "display":
+                if method_type in interface[cmd].keys():
+                    interface[cmd][method_type]().display(book)
+                else:
+                    raise ValueError(f"Unknown display type: {method_type}")
+            elif cmd == "print":
+                if method_type in interface[cmd].keys():
+                    interface[cmd][method_type]().print(book)
+                else:
+                    raise ValueError(f"Unknown print type: {method_type}")
+            elif cmd == "serialize":
+                if method_type in interface[cmd].keys():
+                    return interface[cmd][method_type]().serialize(book)
+                else:
+                    raise ValueError(f"Unknown serialize type: {method_type}")
+        else:
+            raise ValueError(f"Invalid command: {cmd}")
 
 
 if __name__ == "__main__":
